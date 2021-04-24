c8fd941bd0c95a35f6bd109f0c16c3adef312404
danhausen <danihausen@live.com>
2021-04-23 23:04:49 -0300

    Aula01_Progress

--- lib/home/widgets/appbar/app_bar_widget.dart
+++ lib/home/widgets/appbar/app_bar_widget.dart
@@ -1,12 +1,51 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_nlw5/core/app_gradients.dart';
+import 'package:flutter_nlw5/core/app_text_styles.dart';
+import 'package:flutter_nlw5/home/widgets/score_card/score_card_widget.dart';
 
 class AppBarWidget extends PreferredSize {
   AppBarWidget()
       : super(
           preferredSize: Size.fromHeight(250),
           child: Container(
+            height: 250,
             decoration: BoxDecoration(gradient: AppGradients.linear),
+            child: Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 20),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.end,
+                children: [
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Text.rich(
+                        TextSpan(
+                          text: 'Olá, ',
+                          style: AppTextStyles.title,
+                          children: [
+                            TextSpan(
+                                text: 'Dan Hausen',
+                                style: AppTextStyles.titleBold),
+                          ],
+                        ),
+                      ),
+                      Container(
+                        height: 58,
+                        width: 58,
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(10),
+                          image: DecorationImage(
+                            image: NetworkImage(
+                                'https://avatars.githubusercontent.com/u/20982207?v=4'),
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                  ScoreCardWidget(),
+                ],
+              ),
+            ),
           ),
         );
 }
---
+++ lib/home/widgets/chart/chart_widget.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+class ChartWidget extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 80,
+      width: 80,
+      child: Stack(
+        children: [
+          Container(
+            height: 80,
+            width: 80,
+            child: CircularProgressIndicator(
+              strokeWidth: 10,
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
